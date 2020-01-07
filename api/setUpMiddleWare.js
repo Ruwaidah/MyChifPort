09f0d4abe7e75c8f91dbd10d8d7b929e1597ee7b
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-01-06 20:51:16 -0600

    image upload

--- api/setUpMiddleWare.js
+++ api/setUpMiddleWare.js
@@ -9,5 +9,16 @@ module.exports = server => {
   server.use(express.static("uploads"));
   server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method == "OPTIONS") {
+      res.header(
+        "Access-Control_Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
+    }
   });
 };
